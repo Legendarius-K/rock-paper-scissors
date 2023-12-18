5ac411b39be3d3e6c5dccd747cf8ee812eee3ba2
Darius K <darius.kaya@gmail.com>
2023-12-18 14:17:33 +0100

    init commit

---
+++ index-rps.js
@@ -0,0 +1,21 @@
+/*
+- Bygg frontend
+- 
+
+- user chooses weapon
+- user weapon is highlighted and the unchosen weapns fade out
+- a short delay, then comp randomizes a weapon
+- comp weapon is highlighted and the unchosen weapns fade out
+- the winner weapon get extra highlighted and animates into the middle, 
+
+*/
+
+$(() => {
+
+
+
+
+
+
+    
+})
