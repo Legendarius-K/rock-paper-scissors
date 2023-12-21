0d529a65ceff3ed59222fc0b5dddc39f5d9dffec
Darius K <darius.kaya@gmail.com>
2023-12-21 12:29:18 +0100

    added tons of stuff

--- index-rps.js
+++ index-rps.js
@@ -18,9 +18,27 @@ let userTurn = true;
 let userChoice;
 let compChoice;
 let userWinner;
-let userWinMessage = () => console.log("Human Wins!");
-let compWinMessage =  () => console.log("Comp Wins!");
+let newRound;
+let compScore = 0;
+let userScore = 0;
+let userWinMessage = () => {
+    console.log("Human Wins!");
+    userScore++;
+    setTimeout(() => {
+        $(".scoreboard").text(`${userScore} - ${compScore}`);
+     }, 2200);
+}
+let compWinMessage =  () => {
+    console.log("Comp Wins!");
+    compScore++;
+    setTimeout(() => {
+       $(".scoreboard").text(`${userScore} - ${compScore}`);
+    }, 2200);
+    
+}    
 let tieMessage =  () => console.log("It's a tie!");
+
+$(".scoreboard").text(`${userScore} - ${compScore}`);
 
 const checkWinner = () => {
     if (userChoice === "user-rock active") {
@@ -58,22 +76,65 @@ const checkWinner = () => {
     if (userWinner) {
         $(".user button").each((index, e) => {
             if ($(e).attr('class') === userChoice) {
-                $(e).delay(1000).animate({
-                    width: "70%"
-                }, 500);
+                $(e).delay(2000).animate({width: "55%"}, 500);
+                // $(e).delay(2000).toggleClass("winner");
             }
+            setTimeout(() => {
+                $(".player-message").text("HUMAN WINS!");
+            }, 2000);
         });
     } else if (userWinner === false) {
-        $(".comp img").each((index, e) => {
+        $(".comp button").each((index, e) => {
             if ($(e).attr('class') === compChoice) {
-                $(e).delay(1000).animate({
-                    width: "70%"
-                }, 500);
+                $(e).delay(2000).animate({width: "55%"}, 500);
+                // $(e).delay(2000).toggleClass("winner");
             }
+            setTimeout(() => {
+                $(".player-message").text("COMPUTER WINS!");
+            }, 2000);
         })
+    } else {
+        setTimeout(() => {
+            $(".player-message").text("TIE GAME!");
+            $(".vs-pic").toggle();
+            $(".tie-pic").toggle();
+        }, 2000);
     }
+
 }
 
+if (newRound) {
+    $(".main-content").on("click", () => {
+        $(".vs-pic").toggle();
+        $(".tie-pic").toggle();
+    })
+}
+
+
+const nextRound = () => {
+    $(".player-message").text("Choose your weapon")
+    userWinner = undefined;
+
+    $(".grid-box.user button").each((index, button) => {
+        button.disabled = false;
+        $(button).animate({opacity: "100%"},500);
+        if ($(button).hasClass(userChoice)) {
+            $(button).animate({width: "35%"},200).toggleClass("active");
+        }
+    })
+
+    $(".comp button").each((index, image) => {
+        $(image).animate({opacity: "100%"},500);
+        if ($(image).hasClass(compChoice)) {
+            $(image).animate({width: "35%"},200);
+        }
+    })
+    
+    $(".tie-pic").fadeOut(1);
+    $(".vs-pic").fadeIn();
+
+    
+}
 
 
 $(".grid-box.user button").on("click", e => {
@@ -87,25 +148,30 @@ $(".grid-box.user button").on("click", e => {
     $(e.target).siblings().addBack().prop('disabled', true);
 
     if (userTurn === false) {
-        setTimeout(1000);
         compChoice = weapons[Math.floor(Math.random() * weapons.length)];
         console.log(compChoice);
-        $(".comp img").each((index, element) => {
+        $(".comp button").each((index, element) => {
             if ($(element).hasClass(compChoice)) {
 
             } else {
-                $(element).fadeTo("slow", 0.25);
+                $(element).delay(1000).fadeTo("slow", 0.25);
             }
         })
     }
 
     checkWinner();
-    
 
+    newRound = true;
+    
 })
 
-
-
+$(".next-round").on("click", () => nextRound());
+$(".new-game").on("click", () => {
+    nextRound();
+    userScore = 0;
+    compScore = 0;
+    $(".scoreboard").text(`${userScore} - ${compScore}`);
+});
 
 
 })
